b50628c2849545857c1949881e9ada74ae9c459f
Rome Ojuro Otieno <otienorome2@gmail.com>
2025-01-22 00:19:53 +0300

    First sttempt

--- server/app.py
+++ server/app.py
@@ -46,6 +46,25 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        """Handle PATCH request to update a plant"""
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return jsonify(plant.to_dict()), 200
+
+    def delete(self, id):
+        """Handle DELETE request to remove a plant"""
+        plant = Plant.query.get_or_404(id)
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
